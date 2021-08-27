05b2018b856f198b151e0774242417664e4ddf1b
msolorio <msolorio.github@gmail.com>
2021-08-26 22:41:16 -0700

    Adds code for storymaker

---
+++ README.md
@@ -0,0 +1,6 @@
+# Story Maker
+
+- Run the program - `python3 main.py`
+- When prompted enter a first line to your story and press enter.
+- GPT-3 will respond by adding to your story.
+- Press spacebar to have GPT-3 add more.
---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import openai
+import keyboard
+from dotenv import load_dotenv
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+restart_sequence = ""
+
+total_text = input('Please provide an opening line: ')
+
+
+while True:
+  response = openai.Completion.create(
+    engine="davinci",
+    prompt=total_text,
+    temperature=0.9,
+    max_tokens=50,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+
+  text_response = response.choices[0].text
+  total_text += text_response
+
+  print(text_response, end='')
+
+  keyboard.wait('space')
